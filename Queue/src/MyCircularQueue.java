a611cdc65fcd88e1519983e8ca9fc83b15093dd8
Tianyng-Yang <yty1997@outlook.com>
2018-08-30 10:09:29 -0400

    queue

--- Queue/src/MyCircularQueue.java
+++ Queue/src/MyCircularQueue.java
@@ -1,65 +1,69 @@
 class MyCircularQueue {
     
-    int size;
-    List<Integer> data;
-    int head, tail;
+    private int[] data;
+    private int head;
+    private int tail;
+    private int size;
 
     /** Initialize your data structure here. Set the size of the queue to be k. */
     public MyCircularQueue(int k) {
+        data = new int[k];
+        head = -1;
+        tail = -1;
         size = k;
-        head = 0; 
-        tail = 0;
-        data = new ArrayList<Integer>();
     }
     
     /** Insert an element into the circular queue. Return true if the operation is successful. */
     public boolean enQueue(int value) {
-        if(isFull()) return false;
-        if(tail == size-1){
-            tail = 0;
-            data[tail] = value;
+        if (isFull() == true) {
+            return false;
         }
-        else{
-            tail += 1;
-            data[tail] = value;
+        if (isEmpty() == true) {
+            head = 0;
         }
+        tail = (tail + 1) % size;
+        data[tail] = value;
         return true;
     }
     
     /** Delete an element from the circular queue. Return true if the operation is successful. */
     public boolean deQueue() {
-        if(isEmpty()) return false;
-        data[head] = null;
-        if(head == size-1){
-            head = 0;
+        if (isEmpty() == true) {
+            return false;
         }
-        else{
-            head += 1;
+        if (head == tail) {
+            head = -1;
+            tail = -1;
+            return true;
         }
+        head = (head + 1) % size;
         return true;
     }
     
     /** Get the front item from the queue. */
     public int Front() {
+        if (isEmpty() == true) {
+            return -1;
+        }
         return data[head];
     }
     
     /** Get the last item from the queue. */
     public int Rear() {
+        if (isEmpty() == true) {
+            return -1;
+        }
         return data[tail];
     }
     
     /** Checks whether the circular queue is empty or not. */
     public boolean isEmpty() {
-        if(head == tail) return true;
-        return false;
+        return head == -1;
     }
     
     /** Checks whether the circular queue is full or not. */
     public boolean isFull() {
-        if((head > 0 && head-1 == tail)||(head == 0 & tail == size-1)) 
-            return true;
-        return false;
+        return ((tail + 1) % size) == head;
     }
 }
 
@@ -73,3 +77,25 @@ class MyCircularQueue {
  * boolean param_5 = obj.isEmpty();
  * boolean param_6 = obj.isFull();
  */
+
+/*********************************** Built-in Queue Method ********************************************/
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. Initialize a queue.
+        Queue<Integer> q = new LinkedList();
+        // 2. Get the first element - return null if queue is empty.
+        System.out.println("The first element is: " + q.peek());
+        // 3. Push new element.
+        q.offer(5);
+        q.offer(13);
+        q.offer(8);
+        q.offer(6);
+        // 4. Pop an element.
+        q.poll();
+        // 5. Get the first element.
+        System.out.println("The first element is: " + q.peek());
+        // 7. Get the size of the queue.
+        System.out.println("The size is: " + q.size());
+    }
+}
